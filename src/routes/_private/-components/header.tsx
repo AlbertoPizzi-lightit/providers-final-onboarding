213ca3ccd61efed145951f967427070ff4931e39
Alberto Pizzi <albertopizzi@Albertos-MacBook-Pro.local>
2026-01-13 11:51:39 -0300

    style: prettier

--- src/routes/_private/-components/header.tsx
+++ src/routes/_private/-components/header.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 export const Header = ({ children }: PropsWithChildren) => {
   return (
-    <nav className="border-b-base-border bg-base-background fixed top-0 left-0 z-10 flex w-full flex-wrap border-b p-6">
+    <nav className="fixed top-0 left-0 z-10 flex w-full flex-wrap border-b border-b-base-border bg-base-background p-6">
       {children}
     </nav>
   );
--- src/routes/_private/-components/profile-picture.tsx
+++ src/routes/_private/-components/profile-picture.tsx
@@ -1,7 +1,11 @@
+import { useTranslation } from "react-i18next";
+
 export const ProfilePicture = () => {
+  const { t } = useTranslation();
+
   return (
-    <div className="bg-button flex size-15 items-center justify-center rounded-full text-white">
-      AS
+    <div className="flex size-15 items-center justify-center rounded-full bg-button text-white">
+      {t("providers.profile.name")}
     </div>
   );
 };
